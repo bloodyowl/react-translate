b889ecb164d82d8ccbd337b1c8020a6ea693dc2b
b1rd <notverysmartbird@gmail.com>
2017-12-21 11:47:00 +0300

    Move to prop-types package

--- src/TranslatorProvider.js
+++ src/TranslatorProvider.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes, Children } from "react"
+import PropTypes from 'prop-types';
+import React, { Component, Children } from "react";
 import createTranslator from "./createTranslator"
 
 class TranslatorProvider extends Component {
